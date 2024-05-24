9f8768abf82e28e375d8f84c7b12a40ad6fd3b9c
Aayush <aayush.byanjankar28@gmail.com>
2024-05-24 14:32:28 -0500

    topkfreq

--- array&hashing/topKfrequency.js
+++ array&hashing/topKfrequency.js
@@ -16,5 +16,24 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    
+    let count = {}
+    let freq = Array.from({length : nums.length + 1}, () => []);
+
+    for(const n of nums){
+        count[n] = (count[n] || 0) + 1;
+    }
+    for(const n in count){
+        freq[count[n]].push(parseInt(n));
+    }
+
+    let res = []
+    for(let i = freq.length -1; i > 0; i--){
+        for(const n of freq[i]){
+            res.push(n) 
+            if(res.length === k){
+                return res; 
+            }
+        }
+    }
 };
+
