7871d9472ea0b410aec38bc93a45b9946f677582
Aayush <aayush.byanjankar28@gmail.com>
2024-07-04 10:23:27 -0500

    heap

---
+++ heap/taskScheduling.py
@@ -0,0 +1,15 @@
+# Task Scheduling
+# You are given an array of CPU tasks tasks, where tasks[i] is an uppercase english character from A to Z. You are also given an integer n.
+
+# Each CPU cycle allows the completion of a single task, and tasks may be completed in any order.
+
+# The only constraint is that identical tasks must be separated by at least n CPU cycles, to cooldown the CPU.
+
+# Return the minimum number of CPU cycles required to complete all tasks.
+
+# Example 1:
+
+# Input: tasks = ["X","X","Y","Y"], n = 2
+
+# Output: 5
+
