409b3cde928bd6ae0ca1333576ad7a9ea72db226
Aayush <aayush.byanjankar28@gmail.com>
2024-07-10 19:15:12 -0500

    dp

--- 1D-DP/stairs.py
+++ 1D-DP/stairs.py
@@ -14,3 +14,12 @@
 # 2 = 2
 
 
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1 
+
+        for i in range(n-1):
+            temp = one 
+            one = one + two 
+            two = temp 
+        return one 
